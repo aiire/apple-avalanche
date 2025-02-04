a7b85abc7467646f88f1c6517c92d7d4cafe6bef
aiire <104533992+aiire@users.noreply.github.com>
2025-02-04 08:45:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,60 +12,77 @@ wn.bgpic("background.gif")
 wn.addshape(apple_image) # Make the screen aware of the new file
 wn.addshape(pear_image) 
 
-drawer = trtl.Turtle()
-apple = trtl.Turtle()
-
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
-  active_apple.shape(apple_image)
-  wn.update()
+	active_apple.shape(apple_image)
+	wn.update()
   
-letters = ["a", "b", "c", "d", "e", "f"]
-def make_turtles():
-  turtles = []
-  for _ in range(3):
-    t = trtl.Turtle()
-    t.penup()
-    draw_apple(t)
-    t.goto(random.randint(-100, 100), random.randint(-10, 90))
-    turtles.append(t)
-  return turtles
+letters = ["a", "s", "d", "f"]
+def make_apple_turtles():
+	turtles = []
+	for _ in range(len(letters)):
+		t = trtl.Turtle()
+		t.penup()
+		draw_apple(t)
+		t.goto(random.randint(-100, 100), random.randint(-10, 90))
+		turtles.append(t)
+	return turtles
+
+def draw_letter(x, y, drawer, letter):
+	drawer.goto(x, y)
+	drawer.color("white")
+	drawer.write(letter.upper(), font=("Arial", 40, "bold"))
 
 def draw_letters(letters, turtles):
-  drawers = []
-  for i, turtle in enumerate(turtles):
-    drawer = trtl.Turtle()
-    drawer.penup()
-    drawer.hideturtle()
-    drawer.goto(turtle.xcor()-18, turtle.ycor()-35)
-    drawer.write(letters[i], font=("Arial", 40, "bold"))
-    drawers.append(drawer)
-  return drawers
+	drawer_turtles = []
+	for i, letter in enumerate(letters):
+		drawer = trtl.Turtle()
+		drawer.penup()
+		drawer.hideturtle()
+		turtle = turtles[i]
+		draw_letter(turtle.xcor()-10, turtle.ycor()-40, drawer, letter)
+		drawer_turtles.append(drawer)
+	return drawer_turtles
   
 
-def draw_an_A(drawer):
-  drawer.hideturtle()
-  drawer.goto(-10, -40)
-  drawer.color("white")
-  drawer.write("A", font=("Arial", 40, "bold"))
+# def draw_an_A(drawer):
+#   drawer.hideturtle()
+#   drawer.goto(-10, -40)
+#   drawer.color("white")
+#   drawer.write("A", font=("Arial", 40, "bold"))
 
-def erase_an_A():
-  global drawer
-  drawer.clear()
+# def erase_an_A():
+#   global drawer
+#   drawer.clear()
 
-def apple_fall():
-  global apple
-  x = apple.xcor()
-  y = apple.ycor()
-  apple.penup()
-  apple.goto(x, y-140)
-  erase_an_A()
- 
+
+
+
+def erase_letter(index):
+	global drawer_turtles
+	drawer = drawer_turtles[index]
+	drawer.clear()
+
+def apple_fall(index):
+	turtle = apple_turtles[index]
+	drawer = drawer_turtles[index]
+	letter = letters[index]
+	x = turtle.xcor()
+	y = turtle.ycor()
+	turtle.penup()
+	turtle.goto(x, y-140)
+	erase_letter(index)
+	draw_letter(turtle.xcor()-10, turtle.ycor()-40, drawer, letter)
+
 #-----function calls-----
-apples = make_turtles()
-draw_letters(letters, apples)
-wn.onkeypress(apple_fall, "a")
+apple_turtles = make_apple_turtles()
+drawer_turtles = draw_letters(letters, apple_turtles)
+
+wn.onkeypress(lambda: apple_fall(0), 'a')
+wn.onkeypress(lambda: apple_fall(1), 's')
+wn.onkeypress(lambda: apple_fall(2), 'd')
+wn.onkeypress(lambda: apple_fall(3), 'f')
 
 wn.listen()
 wn.mainloop()
